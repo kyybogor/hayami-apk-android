cb16e566a6d69066030cad8c7b072d28e7a3c1b6
kyybogor <kyybogor001@gmail.com>
2025-06-13 09:14:55 +0700

    iki

---
+++ lib/pos/posscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Posscreen extends StatefulWidget {
+  const Posscreen({super.key});
+
+  @override
+  State<Posscreen> createState() => _PosscreenState();
+}
+
+class _PosscreenState extends State<Posscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
